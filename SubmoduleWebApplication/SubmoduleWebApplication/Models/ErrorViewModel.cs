f947d25a35689c993d4f990301349ff12449b5ff
Brian L. Gorman <blgorman@gmail.com>
2020-06-20 22:15:56 -0500

    intial web app commit

---
+++ SubmoduleWebApplication/SubmoduleWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SubmoduleWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
